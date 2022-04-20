870edbb6f0cec3c5030d0703af87d1409bf578d1
guorong.zheng <guorong.zheng@talebase.com>
2022-04-20 18:09:48 +0800

    deatil's style

--- components/DetailCard.vue
+++ components/DetailCard.vue
@@ -1,44 +1,60 @@
 <template>
-  <div class="transition duration-300 ease-in-out">
+  <div
+    :class="`transition duration-300 ease-in-out ${
+      app ? 'w-full' : 'w-2/4 mx-auto'
+    } my-6`"
+  >
     <el-row>
-      <el-col :span="12">
-        <Card>
-          <div>
+      <el-col :span="app ? 12 : 24" :offset="app ? 6 : 0">
+        <Card class="h-full">
+          <div class="dark:text-gray-400">
             <i :class="iconfontName"></i>
-            <span>{{ $t(title) }}</span>
+            <span class="pl-2">{{ $t(title) }}</span>
           </div>
-          <div>
+          <div class="pt-2">
             <template v-if="mode">
-              <div v-for="(item, index) in data" :key="index">
-                <span>{{ $t(item.title) }}</span>
-                <span>{{ item.content }}</span>
+              <div
+                v-for="(item, index) in data"
+                :key="index"
+                class="flex justify-between py-1"
+              >
+                <span class="font-bold text-gray-700 dark:text-gray-500">{{
+                  $t(item.title)
+                }}</span>
+                <span class="dark:text-gray-400">{{ item.content }}</span>
               </div>
             </template>
             <template v-else>
-              <div v-for="(item, index) in data" :key="index">
+              <div
+                v-for="(item, index) in data"
+                :key="index"
+                class="flex justify-between py-1"
+              >
                 <template v-if="!element">
-                  <span>{{ item.split(':')[0] }}</span>
-                  <span>{{ item.split(':')[1] }}</span>
+                  <span class="font-bold text-gray-700 dark:text-gray-500">{{
+                    item.split(':')[0]
+                  }}</span>
+                  <span class="dark:text-gray-400">{{ item.split(':')[1] }}</span>
                 </template>
                 <template v-else>
-                  <span>{{ index }}</span>
-                  <span>{{ item }}</span>
+                  <span class="font-bold text-gray-700 dark:text-gray-500">{{ index }}</span>
+                  <span class="dark:text-gray-400">{{ item }}</span>
                 </template>
               </div>
             </template>
           </div>
         </Card>
       </el-col>
-      <el-col v-if="app" :span="5">
-        <Card>
+      <el-col v-if="app" :span="4" :offset="1">
+        <Card class="text-center mx-auto">
           <div>
             <i class="iconfont iconsaoma"></i>
-            <span>{{ $t('label.app') }}</span>
+            <span class="text-xs">{{ $t('label.app') }}</span>
           </div>
           <img
             src="~/assets/img/code.jpg"
             alt="小程序码"
-            style="width: 160px; height: 160px"
+            class="w-2/3 h-2/3 mx-auto my-3"
           />
         </Card>
       </el-col>
