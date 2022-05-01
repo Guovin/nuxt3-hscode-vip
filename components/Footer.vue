ff057a2c6621d5a7316d1a978fa5de293a865c5e
Govin <govin@GovindeMacBook-Pro.local>
2022-05-01 11:32:31 +0800

    fix:el-drawer in ssr

--- components/Footer.vue
+++ components/Footer.vue
@@ -29,32 +29,36 @@
         target="blank"
         >{{ $t('footer.siteId') }}</a
       >
-      <el-drawer
-        v-model="drawer"
-        :title="$t('footer.statement')"
-        direction="btt"
-      >
-        <div class="text-justify text-gray-600 px-4 sm:px-16 dark:text-gray-300">
-          <p class="mb-1">
-            {{ $t('footer.statementContent1') }}
-          </p>
-          <p class="mb-1">
-            {{ $t('footer.statementContent2') }}
-          </p>
-          <p class="mb-1">
-            {{ $t('footer.statementContent3') }}
-          </p>
-          <p class="mb-1">
-            {{ $t('footer.statementContent4') }}
-          </p>
-          <p class="mb-1">
-            {{ $t('footer.statementContent5') }}
-          </p>
-          <p class="mb-1">
-            {{ $t('footer.statementContent6') }}
-          </p>
-        </div>
-      </el-drawer>
+      <client-only>
+        <el-drawer
+          v-model="drawer"
+          :title="$t('footer.statement')"
+          direction="btt"
+        >
+          <div
+            class="text-justify text-gray-600 px-4 sm:px-16 dark:text-gray-300"
+          >
+            <p class="mb-1">
+              {{ $t('footer.statementContent1') }}
+            </p>
+            <p class="mb-1">
+              {{ $t('footer.statementContent2') }}
+            </p>
+            <p class="mb-1">
+              {{ $t('footer.statementContent3') }}
+            </p>
+            <p class="mb-1">
+              {{ $t('footer.statementContent4') }}
+            </p>
+            <p class="mb-1">
+              {{ $t('footer.statementContent5') }}
+            </p>
+            <p class="mb-1">
+              {{ $t('footer.statementContent6') }}
+            </p>
+          </div>
+        </el-drawer>
+      </client-only>
       <FeedBack
         :visiable="feedBackVisiable"
         @fbDialogVisiable="fbDialogVisiable"
